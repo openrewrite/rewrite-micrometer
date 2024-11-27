0de88c69df97938cb7d2ade6bc5f5020f31f9541
Tim te Beek <tim@moderne.io>
2024-11-27 17:01:56 +0100

    Add testImplementation("org.openrewrite:rewrite-test")

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     testImplementation("org.openrewrite:rewrite-gradle")
     testImplementation("org.openrewrite:rewrite-maven")
 
+    testImplementation("org.openrewrite:rewrite-test")
     testImplementation("org.openrewrite:rewrite-kotlin:latest.release")
 
     testImplementation("org.junit.jupiter:junit-jupiter-engine:latest.release")
